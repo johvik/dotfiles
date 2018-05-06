4634f3f13638688a8228bddf27135620db0fee57
johvik <johvik@users.noreply.github.com>
2018-05-06 22:11:49 +0200

    Create user.js

---
+++ user.js
@@ -0,0 +1,7 @@
+user_pref("dom.push.enabled", false);
+user_pref("dom.webnotifications.enabled", false);
+user_pref("dom.webaudio.enabled", false);
+user_pref("geo.enabled", false);
+user_pref("browser.pocket.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("media.autoplay.enabled", false);
